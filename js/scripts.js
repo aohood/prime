373d30691d4f9710e83a5cf4d4c7c90be960cd98
Aya Abu <someone990@yahoo.com>
2019-03-07 00:34:24 -0800

    finish the prime sifting

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,34 @@
 $(document).ready(function() {
 
-  $("form#cal").submit(function(event) {
+  $("form#prime").submit(function(event) {
     event.preventDefault();
-    var number1= parseFloat($("#num1").val());
-    var number2 = parseFloat($("#num2").val());
+    $("#result").show();
+    var input = parseInt($("#num").val());
 
+    var prime= new Array();
+    // var divisble=2;
 
-
-
-    if( $("#operation").val() === "adding"){
-      alert("The result of "+($("#add").val())+" " +
-      ($("#num1").val()) +
-      " and "+ ($("#num2").val())+ " is "+add(number1,number2));
+    for (i=2; i<=input; i++){
+      prime.push(i);
     }
-      else if ($("#operation").val() === "subtracting"){
-        alert("The result of "+($("#sub").val())+" " +
-        ($("#num2").val()) +
-        " from "+($("#num1").val()) + " is "+sub(number1,number2));
-      }
-      else if ($("#operation").val() === "multiplying") {
-        alert("The result of "+($("#mul").val())+" " +
-        ($("#num1").val()) +
-        " and "+ ($("#num2").val())+ " is "+multiply(number1,number2));
-      }
-      else if (($("#operation").val() === "dividing")){
-        alert("The result of "+($("#divide").val())+" " +
-        ($("#num1").val()) +
-        " by "+ ($("#num2").val())+ " is "+divide(number1,number2));
+    for (divisble=2; divisble<=input; divisble++){
+      for(i=0; i<input-2; i++){
+        if ((prime[i] !== divisble) && (prime[i] % divisble === 0)){
+          prime.splice(i,1);
+          // i--; // to account for the popes elements
+        }
 
-      }
+      } //for i
+    } //for divisble
 
-
-
-
-
-
-
-
-
-
-
-    // $("#sub").click(function(){
-    //   alert(sub(number1,number2));
-    // }); //for the click
-    //
-    // $("#mul").select(function(){
-    //   multiply(number1,number2);
-    // }); //for the click
-    //
-    // $("#divide").select(function(){
-    //   divide(number1,number2);
-    // }); //for the click
+    var index=0;
+    $("#result").empty();
+    prime.forEach(function(){
+      $("#result").append("<li>"+prime[index]+"</li>");
+      index++;
+    });
 
   }); //for the submit
+
+
 }); //for the document
-
-
-
-
-  var add = function(n,m){
-    return n+m;
-  }
-  var sub = function(n,m){
-    return n-m;
-  }
-  var multiply = function(n,m){
-    return n*m;
-  }
-  var divide = function(n,m){
-    return n/m;
-  }
